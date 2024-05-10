7c06a0abf5ef28e55d0925915ce16386f8aa3d57
Mateo-mh <byronmh8@gmail.com>
2024-05-10 15:00:45 -0500

    rutas protegidas creadas

---
+++ devter/src/app/login/page.tsx
@@ -0,0 +1,8 @@
+import { AuthButtonServer } from "../components/auth-button-server";
+
+export function Login() {
+    <section className="grid place-conten-center min-h-screen">
+        <h1 className="txt-xl font-bold">Inicia sesión en Devter</h1>
+        <AuthButtonServer />
+    </section>
+}
