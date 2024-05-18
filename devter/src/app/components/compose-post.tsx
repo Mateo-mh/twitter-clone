4f86df1108dd9b05205b6ca6456c32b871945b4d
Mateo-mh <byronmh8@gmail.com>
2024-05-17 19:52:55 -0500

    Proyecto terminado en un 90%

--- devter/src/app/components/compose-post.tsx
+++ devter/src/app/components/compose-post.tsx
@@ -1,37 +1,32 @@
-import {  createServerActionClient  } from "@supabase/auth-helpers-nextjs"
-import {  cookies } from "next/headers"
-import { revalidatePath } from "next/cache"
-import { ComposePostTextArea } from "./compose-post-text-area";
+'use client'
 
-export function ComposePost({
-    userAvatarUrl
-}:{
-    userAvatarUrl:string
+import { addPost } from '../actions/add-post-action'
+import { useRef } from 'react'
+
+export function ComposePost ({
+  userAvatarUrl
+}: {
+  userAvatarUrl: string
 }) {
-    const addPost = async (formData: FormData) => {
-        'use server'
-        const content = formData.get("content")
+  const formRef = useRef<HTMLFormElement>(null)
 
-        if(content == null) return
-
-        const supabase = createServerActionClient({cookies})
-        //revisar si el usuario esta autentificado
-        const { data: {user}} = await supabase.auth.getUser()
-        if(user == null) return
-        await supabase.from('posts').insert({ content, user_id: user.id })
-
-        revalidatePath('/')
-        
-    }
-    return (
-        <form action={addPost} className="flex flex-row p-4 border-b border-gray-300">
-            <img className="w-10 h-10 rounded-full object-contain mr-4" src={userAvatarUrl}/>
-            <div className="flex flex-1 flex-col gap-y-4">
-                <ComposePostTextArea />
-                <button type="submit" className="bg-[#1D9BF0] text-sm text-white hover:bg-[#2A8EFF] font-bold rounded-full px-5 py-2 self-end">
-                    Postear
-                </button>
-            </div>
-        </form>
-    )
+  return (
+    <form ref={formRef} action={async (formData) => {
+      await addPost(formData)
+      formRef.current?.reset()
+    }} className='flex flex-row p-3 border-b border-gray-300'>
+      <img className='rounded-full w-10 h-10 object-contain mr-4' src={userAvatarUrl} />
+      <div className='flex flex-1 flex-col gap-y-4'>
+      <textarea
+        name='content'
+        rows={4}
+        className='w-full text-xl bg-gray-300/10 placeholder-gray-500 p-2'
+        placeholder='¡¿Qué está pasando?!'
+      ></textarea>
+        <button type="submit" className="bg-[#1D9BF0] text-sm text-white hover:bg-[#2A8EFF] font-bold rounded-full px-5 py-2 self-end">
+            Postear
+        </button>
+      </div>
+    </form>
+  )
 }
